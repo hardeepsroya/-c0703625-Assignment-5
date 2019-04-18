8bd176faeff2571e9f23414b745f90b37ea1606a
visualstudio <visualstudio@230-PC34>
2019-04-18 14:09:36 -0400

    2nd commit

--- c0703625/Program.cs
+++ c0703625/Program.cs
@@ -10,9 +10,43 @@ namespace c0703625
     {
         static void Main(string[] args)
         {
+
             Program a = new Program();
-            Console.WriteLine("the longest word is:");
-            Console.WriteLine(a.MyFunction A("Ada lovelace wrote the first algorithm designed for processing by an analytical engine.")); }
+
+            Console.WriteLine("the longest word is : ");
+
+            Console.WriteLine(a.MyFunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine."));
+
+            Console.ReadLine();
         }
+
+        public String MyFunctionA(string input)
+        {
+            string[] words = input.Split(' ');
+
+            int wordArrayLength = words.Length;
+
+            int[] wordsLength = new int[wordArrayLength];
+
+            foreach (var word in words)
+            {
+
+            }
+
+            string currentWord = words[0];
+
+            string nextWord;
+
+            for (int y = 0; y < words.Length - 1; y++)
+            {
+                currentWord = words[y];
+
+                nextWord = words[y + 1];
+
+            }
+
+            return currentWord;
+        }
+
     }
 }
